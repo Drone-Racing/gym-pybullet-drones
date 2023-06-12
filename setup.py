1549372c62cb5372757bde7ad3fc2e11a560cc83
Yvo Keuter <ypmkeuter@gmail.com>
2023-06-12 12:03:07 +0200

    colcon support

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup
 
+package_name = 'gym_pybullet_drones'
 
 setup(
-    name = "gym-pybullet-drones",
-    version = "1.0.0",
-    description = "PyBullet Gym environments for single and multi-agent reinforcement learning of quadcopter control",
-    maintainer = "Jacopo Panerati <jacopo.panerati@utoronto.ca>",
-    license = "MIT",
-    packages=["gym_pybullet_drones"],
+    name=package_name,
+    version='1.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Jacopo Panerati',
+    maintainer_email='jacopo.panerati@utoronto.ca',
+    description='PyBullet Gym environments for single and multi-agent reinforcement learning of quadcopter control',
+    license='MIT',
+    tests_require=['pytest'],
 )
