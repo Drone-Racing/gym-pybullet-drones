fbc7e4efce68475473b7243f9934392cb4bf172c
Yvo Keuter <ypmkeuter@gmail.com>
2023-06-12 11:24:27 +0200

    back to setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name = "gym-pybullet-drones",
+    version = "1.0.0",
+    description = "PyBullet Gym environments for single and multi-agent reinforcement learning of quadcopter control",
+    maintainer = "Jacopo Panerati <jacopo.panerati@utoronto.ca>",
+    license = "MIT",
+    packages=["gym_pybullet_drones"],
+)
